0043587d5d03ef4d99ec476a33dec343f777460f
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-07-10 16:39:00 -0400

    finish example 1

---
+++ 7-POOPF/Ex-1/ex-1.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    firstName = 'Joe'
+    lastName = 'Szaf'
+    age = 28
+
+def main():
+    print("    ********** Welcome to the Employee Database **********    ")
+    print('Fist name:', Employee.firstName)
+    print('Last name:', Employee.lastName)
+    print('Age:', Employee.age)
+
+main()
